253d02ae9d757b1a4af0f388e909649d9791cce2
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-03-10 02:20:04 -0700

    added functionality to both the number box but also the check boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowCB = document.querySelector("#lowerCB");
+var upCB = document.querySelector("#upperCB");
+var numCB = document.querySelector("#numericCB");
+var specCB = document.querySelector("#specialCB");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+//switch statement to keep my code for the handlers dry.
+function trueFalseSwitch(input) {
+  if (input.value == "false") {
+    input.value = "true";
+  } else if (input.value == "true") {
+    input.value = "false";
+  }
+}
+//Event Listeners for the checkboxes
+
+lowCB.addEventListener("click", function () {
+  trueFalseSwitch(lowCB);
+});
+
+upCB.addEventListener("click", function () {
+  trueFalseSwitch(upCB);
+});
+
+numCB.addEventListener("click", function () {
+  trueFalseSwitch(numCB);
+});
+
+specCB.addEventListener("click", function () {
+  trueFalseSwitch(specCB);
+});
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+  var charNum = document.querySelector("#numOfChars").value;
+  var checkboxValues = document.querySelector(".checkbox-footer");
+  alert(checkboxValues.children[3].value);
+  if (charNum !== "") {
+    writePassword(charNum);
+  } else {
+    alert("No input for how many characters you'd like");
+  }
+});
