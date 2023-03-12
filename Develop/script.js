6aa487fa2e0d287e1b81286960ac2d96c80f611a
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-03-12 00:53:46 -0700

    completed setup for the generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,106 @@ var lowCB = document.querySelector("#lowerCB");
 var upCB = document.querySelector("#upperCB");
 var numCB = document.querySelector("#numericCB");
 var specCB = document.querySelector("#specialCB");
+var lowerArray = [
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  w,
+  x,
+  y,
+  z,
+];
+var upperArray = [
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z,
+];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArray = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var selectorArray = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+function generatePassword() {
+  var password;
 }
 
 //switch statement to keep my code for the handlers dry.
@@ -20,6 +114,7 @@ function trueFalseSwitch(input) {
     input.value = "false";
   }
 }
+
 //Event Listeners for the checkboxes
 
 lowCB.addEventListener("click", function () {
@@ -42,7 +137,6 @@ specCB.addEventListener("click", function () {
 generateBtn.addEventListener("click", function () {
   var charNum = document.querySelector("#numOfChars").value;
   var checkboxValues = document.querySelector(".checkbox-footer");
-  alert(checkboxValues.children[3].value);
   if (charNum !== "") {
     writePassword(charNum);
   } else {
